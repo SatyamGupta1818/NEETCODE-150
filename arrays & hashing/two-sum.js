3b0459803ad213cb67ccae457e0927923e71ad81
Satyam Gupta <officialguptasatyam@gmail.com>
2026-08-01 21:09:18 +0530

    Two Sum

---
+++ arrays & hashing/two-sum.js
@@ -0,0 +1,53 @@
+// Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+// You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+// Return the answer with the smaller index first.
+
+// Example 1:
+
+// Input: 
+// nums = [3,4,5,6], target = 7
+
+// Output: [0,1]
+// Explanation: nums[0] + nums[1] == 7, so we return [0, 1].
+
+// Example 2:
+
+// Input: nums = [4,5,6], target = 10
+
+// Output: [0,2]
+
+// Example 3:
+
+// Input: nums = [5,5], target = 10
+
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 1000
+// -10,000,000 <= nums[i] <= 10,000,000
+// -10,000,000 <= target <= 10,000,000
+// Only one valid answer exists.
+
+
+
+
+// const nums = [3, 4, 5, 6];
+const nums = [4,5,6];
+const target = 10;
+
+function twoSum(nums, target) {
+    const numMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
+        }
+        numMap.set(nums[i], i);
+    }
+    return null;
+}
+const result = twoSum(nums, target);
+console.log(result); 
